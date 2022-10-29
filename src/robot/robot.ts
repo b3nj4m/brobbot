465860af4f3d78ff73421131a3a049dfc8599556
Ben Robbins <ben.robbins@gmail.com>
2022-10-29 12:44:00 -0700

    extra bot args

--- src/robot/robot.ts
+++ src/robot/robot.ts
@@ -37,6 +37,8 @@ export default class Robot {
     this.app = new App({
       token: process.env.SLACK_BOT_TOKEN,
       signingSecret: process.env.SLACK_SIGNING_SECRET,
+      appToken: process.env.SLACK_APP_TOKEN,
+      socketMode: true,
       logLevel: LogLevel.DEBUG,
     });
 
